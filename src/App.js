9ee6367daebf5afe52f77f9c958f8985c30d49ad
Randy Ortiz <Randyortiz28@gmail.com>
2021-02-17 18:54:54 -0500

    Created states and pulled Nasa data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from 'axios'
+import Container from './Container'
 
 function App() {
+
+    const [photo, setPhoto] = useState(null)
+    const [date, setDate] = useState(null)
+
+    
+    useEffect(() => {
+      const fetchData = () => {
+        axios
+          .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
+      fetchData()
+    }, [])
+  
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <Container/>
   );
 }
 
---
+++ src/Container.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Container() {
+
+
+    return(
+        <>
+        <div>
+            <h1>Astronomy Picture of the Day </h1>
+            <p>lorem ipsum</p>
+            <p>date</p>
+        </div>
+        <div>
+            <img src="" alt="Nasa-img"></img>
+        </div>
+        <div>
+            <h5></h5>
+            <h5></h5>
+            <p></p>
+            <h5></h5>
+            <h5></h5>
+        </div>
+        </>
+    )
+}
+
+export default Container;
