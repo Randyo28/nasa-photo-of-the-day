6c4418374d7c0c66377d4c5b3d964fbab81fc4b3
Randy Ortiz <Randyortiz28@gmail.com>
2021-02-18 22:21:41 -0500

    Changed my CSS to styled Components

--- src/Container.js
+++ src/Container.js
@@ -1,25 +1,74 @@
 import React from "react";
 import './index.css';
+import styled from 'styled-components'
 
 const Container = (props) => {
+
+    const HeadingDiv = styled.div`
+        text-align: center;
+
+    a{
+        font-weight: 800;
+    margin-right: 10px;
+    text-decoration: none;
+    color:rgb(35, 218, 218);
+    }
+    `
+    const ImageDiv = styled.div`
+        width: 85%;
+        margin: 0 auto;
+
+        img{
+            width: 100%;
+            border-radius: 10%;
+        }
+    `
+
+    const UnderImg = styled.div`
+        text-align: center;
+
+        h5{
+            font-size: 1.1rem;
+        }
+
+        span{
+            color:aqua;
+        }
+
+        p{
+            line-height: 2;
+            font-size: 1.2rem;
+            margin: 50px auto;
+            max-width: 100%;
+            width: 80%;
+
+            span{
+                color:rgba(226, 21, 216, 0.986);
+                font-weight: 900;
+            }
+        }
+    `
 
     const {photo} = props
     // console.log(photo)
     return(
         <>
-        <div className="headingDiv">
-            <h1 className="AstronomyH1">Astronomy Picture of the Day </h1>
-            <p><span><a className="discoverLink" href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a></span>Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
+        <HeadingDiv>
+            <h1>Astronomy Picture of the Day </h1>
+            <p>
+            <span>
+                <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a></span>Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
             <p>{photo.date}</p>
-        </div>
-        <div className="imageDiv">
-            <img className='image' src={photo.url} alt="Nasa-img"></img>
-        </div>
-        <div className="underImg">
-            <h5 className='title'>{photo.title} </h5>
-            <h5 className='copyright'>Image Credit &amp; Copyright: <span className="copyrightSpan">{photo.copyright}</span></h5>
-            <p className='explanation'><span className='explanSpan'>Explanation:</span> {photo.explanation}</p>
-        </div>
+        </HeadingDiv>
+        <ImageDiv>
+            <img src={photo.url} alt="Nasa-img"></img>
+        </ImageDiv>
+        <UnderImg>
+            <h5>{photo.title} </h5>
+            <h5>Image Credit &amp; Copyright: <span>{photo.copyright}</span>
+            </h5>
+            <p ><span>Explanation:</span> {photo.explanation}</p>
+        </UnderImg>
         </>
     )
 }
